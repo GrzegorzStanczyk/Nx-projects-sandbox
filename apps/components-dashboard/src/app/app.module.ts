20422538c5380d201338760e0b1f05025b5c001b
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-09-01 00:11:09 +0200

    added components presentation project

---
+++ apps/components-dashboard/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '@nx-projects-sandbox/components';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    ComponentsModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
