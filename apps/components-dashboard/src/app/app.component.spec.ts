9630b0b87e5915b43240124a458b632de04c0d97
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-09-01 17:02:32 +0200

    CUSTOM_ELEMENTS_SCHEMA for components import test

--- apps/components-dashboard/src/app/app.component.spec.ts
+++ apps/components-dashboard/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
