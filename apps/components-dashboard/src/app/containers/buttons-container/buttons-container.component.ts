656ca4fe9a8dd809e1cf77b536097fe5c097fbd5
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-09-01 16:38:12 +0200

    buttons container

---
+++ apps/components-dashboard/src/app/containers/buttons-container/buttons-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-projects-sandbox-buttons-container',
+  templateUrl: './buttons-container.component.html',
+  styleUrls: ['./buttons-container.component.css']
+})
+export class ButtonsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- apps/components-dashboard/src/app/containers/containers.module.ts
+++ apps/components-dashboard/src/app/containers/containers.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonsContainerComponent } from './buttons-container/buttons-container.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    ButtonsContainerComponent
+  ],
+  exports: [
+    ButtonsContainerComponent
+  ]
 })
 export class ContainersModule { }
